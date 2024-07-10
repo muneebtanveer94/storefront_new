16b342e80836bcec5b001c42f085d5916c82842d
muneebtanveer94 <124376047+muneebtanveer94@users.noreply.github.com>
2024-07-10 17:56:31 +0500

    utopia theme

---
+++ utopia/storyboard.js
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { Scene, Storyboard } from 'utopia-api'
+
+export var storyboard = (
+  <Storyboard>
+    <Scene
+      style={{
+        width: 603,
+        height: 794,
+        position: 'absolute',
+        left: 212,
+        top: 128,
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '253px 101px',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <span
+        style={{
+          wordBreak: 'break-word',
+          fontSize: '25px',
+          width: 257,
+          height: 130,
+        }}
+      >
+        Open the insert menu or press the + button in the
+        toolbar to insert components
+      </span>
+    </Scene>
+  </Storyboard>
+  )
